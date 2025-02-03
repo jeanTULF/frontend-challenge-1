6affb4c6283855a7ed4061544c3684834b4d1b1f
Jean <jeanpolo15@gmail.com>
2025-02-02 22:04:57 -0500

    Fix: correccion de boton para eliminar del carrito, cambio para boton al añadir al carrito, correccion de estilos

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fetch('data.json')
         const imageUrl = image.desktop;
         const content = `
             <div class="list_add">
-            <img src=${imageUrl} alt=${category}>             
+                <img src=${imageUrl} alt=${category}>             
                 <button class="list_add-button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
                 <g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/>
@@ -43,26 +43,17 @@ fetch('data.json')
 
         
         function añadirAlCarrito(product) {
-            const existingProduct = productCart.find(item => item.id === product.id);
-            
-            if (existingProduct) {
-                existingProduct.quantity += 1;
-            } else {
-                productCart.push({ ...product, quantity: 1 });
-            }
-            
+            productCart.push({ ...product, quantity: 1 });
             actualizarCarrito();
-        }
-
-
-        // ! Quiza este ya no funcione, buscar funcionalidad 
-        function getTotalQuantityByItem(product) {
-            const item = productCart.find((p) => p.id === product.id);
-            return item ? item.quantity : 0;
         }
         
         function eliminarProductoDelCarrito(id) {
         productCart = productCart.filter(producto => producto.id !== id)
+        }
+
+        function actualizarCantidadTotal() {
+            const totalItems = productCart.reduce((acc, producto) => acc + producto.quantity, 0);
+            totalDeProductos.innerHTML = totalItems;
         }
 
         function actualizarCarrito() {
@@ -71,6 +62,9 @@ fetch('data.json')
                 const productoContainer = document.createElement('div');
                 let totalPriceItem = producto.price * producto.quantity;
                 productoContainer.classList.add('product-item');
+
+                
+                // TODO: cambio de click en el boton en vez de svg
                 productoContainer.innerHTML = `
                 <div class="list-add">
                 <h2 class="product-name">${producto.name}</h2>
@@ -81,7 +75,7 @@ fetch('data.json')
                     </div>
                 </div>
                     <button class="remove-product" data-index="${index}" id=${id}>
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="21" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <path d="M9.70711 8.29289C9.31658 7.90237 8.68342 7.90237 8.29289 8.29289C7.90237 8.68342 7.90237 9.31658 8.29289 9.70711L10.5858 12L8.29289 14.2929C7.90237 14.6834 7.90237 15.3166 8.29289 15.7071C8.68342 16.0976 9.31658 16.0976 9.70711 15.7071L12 13.4142L14.2929 15.7071C14.6834 16.0976 15.3166 16.0976 15.7071 15.7071C16.0976 15.3166 16.0976 14.6834 15.7071 14.2929L13.4142 12L15.7071 9.70711C16.0976 9.31658 16.0976 8.68342 15.7071 8.29289C15.3166 7.90237 14.6834 7.90237 14.2929 8.29289L12 10.5858L9.70711 8.29289Z" fill="#0F1729"></path>
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12Z" fill="#0F1729"></path>
                         </svg>
@@ -89,6 +83,9 @@ fetch('data.json')
                 `;
                 productosCompra.appendChild(productoContainer);
             });
+
+            actualizarCantidadTotal(); 
+            totalCostoProductos.innerHTML = calcularTotal();
             // Mostrar u ocultar la sección de carrito vacío
             const isCartEmpty = productCart.length === 0;
             carritoVacio.classList.toggle('hidden', !isCartEmpty);
@@ -98,10 +95,31 @@ fetch('data.json')
         function calcularTotal() {
             return productCart.reduce((total, producto) => total + (producto.price * producto.quantity), 0).toFixed(2);
         }
-        
-        // TODO: Realizar cambio de boton para aumentar/disminuir cantidades
 
-        botonAñadir.addEventListener('click', () => {
+        botonAñadir.addEventListener('click', (e) => {
+            // Crear nuevo boton y reemplazar
+            const buttonClicked = e.currentTarget;
+            console.log(buttonClicked);
+            const productContainer = buttonClicked.closest('.list_add');  
+            buttonClicked.style.display = 'none';
+            const addButton = document.createElement('button');
+            addButton.classList.add('list_quantity-button');
+            addButton.innerHTML = `
+                <span id="decrement-qty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+                        <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+                    </svg>
+                </span>
+                <span id="item-qty">1</span>
+                <span id="increment-qty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+                        <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+                    </svg>
+                </span>
+            `;
+            productContainer.appendChild(addButton);
+
+            // Añadir producto 
             añadirAlCarrito(producto);
             Swal.fire({
                 toast: true,
@@ -119,33 +137,33 @@ fetch('data.json')
             totalDeProductos.innerHTML = productCart.length;
             totalCostoProductos.innerHTML = calcularTotal()
             actualizarCarrito();
-        });
+        });  
 
         // TODO: refactorizar este codigo para eliminar SOLO item seleccionado 
         
         document.addEventListener('click', (e) => {
-            if (e.target.classList.contains('remove-product')) {
-                const index = e.target.getAttribute('data-index');
-                const product = productCart[index];
-        
-                eliminarProductoDelCarrito(product.id)
-                Swal.fire({
-                    toast: true,
-                    icon: 'info',
-                    title: `Producto eliminado: ${product.name}`,
-                    position: 'top-right',
-                    animation: true,
-                    timer: 1500,
-                    timerProgressBar: true,
-                    showConfirmButton: false,
-                    customClass: {
-                        popup: 'styleAlert',
-                    },
-                })
-            }
-            totalDeProductos.innerHTML = productCart.length;
-            totalCostoProductos.innerHTML = calcularTotal()
-            actualizarCarrito()
+            const button = e.target.closest('.remove-product');
+            if (button) {
+        const index = button.getAttribute('data-index');
+        const product = productCart[index];
+        eliminarProductoDelCarrito(product.id);
+        Swal.fire({
+            toast: true,
+            icon: 'info',
+            title: `Producto eliminado: ${product.name}`,
+            position: 'top-right',
+            animation: true,
+            timer: 1500,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            customClass: {
+                popup: 'styleAlert',
+            },
+        });
+        totalDeProductos.innerHTML = productCart.length;
+        totalCostoProductos.innerHTML = calcularTotal();
+        actualizarCarrito();
+        }
         });
     });
 })
